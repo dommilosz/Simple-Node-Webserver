b5caec4517913a8af9cd79ae9d5723230bb35cbe
dommilosz <dommilosz@gmail.com>
2020-10-28 18:57:18 +0100

    fix

--- src/wsutils.ts
+++ src/wsutils.ts
@@ -1,8 +1,11 @@
 import * as fs from "fs";
 import {checkLogin} from "./auth-handler";
+import mime = require('mime');
 
 export function sendFile(req,res,path,status){
+    let type = mime.getType(path);
     res.writeHead(status)
+    res.setHeader("Content-Type",type)
     res.write(fs.readFileSync(path, {encoding: 'utf-8'}));
     res.end()
 }
